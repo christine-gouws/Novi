e0c436eefa9e4a00e02633be3b895639ebb85d21
christine gouws <christine.gouws@gmail.com>
2021-11-22 16:49:37 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "feed", to: "pages#feed", as: :feed
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users
+  resources :brands, only: [:show]
+  resources :posts, only: [:create]
+  resources :followings, only: [:create, :destroy]
+  resources :likes, only: [:create, :destroy]
+  resources :ratings, only: [:create, :destroy]
+
+
 end
